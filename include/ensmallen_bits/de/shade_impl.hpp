3363d86b1f3a1beb2f8430cf5f419211205210b5
AlirezaNewTwen <132584443+AlirezaNewTwen@users.noreply.github.com>
2023-12-07 18:30:35 +0100

    bk2
    newbk

--- include/ensmallen_bits/de/shade_impl.hpp
+++ include/ensmallen_bits/de/shade_impl.hpp
@@ -16,14 +16,9 @@
 #include "shade.hpp"
 #include <queue>
 #include <omp.h>
+#include <unordered_set>
 
 namespace ens {
-
-struct Archive {
-    arma::mat pop;
-    arma::mat funvalues;
-    size_t NP; // Assuming NP is the archive size
-};
 
 inline SHADE::SHADE(const size_t populationSize ,
               const size_t maxGenerations,
@@ -57,29 +52,24 @@ typename MatType::elem_type SHADE::Optimize(FunctionType& function,
   double p_best_rate = 0.1;
   double arc_rate = 2.0;
   int problem_size = iterate.n_rows;
-  int memory_size = iterate.n_rows;
-  int pop_size = populationSize;
+  int memory_size = problem_size;
+  const int& pop_size = populationSize;
   // Initialize memory_sf, memory_cr, and memory_pos
   arma::vec memory_sf = 0.5 * arma::ones<arma::vec>(memory_size);
   arma::vec memory_cr = 0.5 * arma::ones<arma::vec>(memory_size);
   int memory_pos = 1;
   // Initialize archive
-  std::vector<BaseMatType> archive_pop(0, BaseMatType(problem_size, 0.0));
-  
-
-  arma::vec archive_funvalues = arma::zeros<arma::vec>(0);
   int archive_NP = static_cast<int>(arc_rate * pop_size);
+  std::vector<BaseMatType> archive_pop(0, BaseMatType(problem_size, 0.0));
+  arma::Col<ElemType>      archive_funvalues = arma::zeros<arma::vec>(0);
   //SHADE SHADE SHADE SHADE SHADE SHADE
-
-
 
   // Population matrix. Each column is a candidate.
   std::vector<BaseMatType> population;
   population.resize(populationSize);
   // Vector of fitness values corresponding to each candidate.
   arma::Col<ElemType> fitnessValues;
-	arma::Col<ElemType> fitnessDif;
-    fitnessDif.set_size(populationSize);
+  fitnessValues.set_size(populationSize);
 
 
   // Make sure that we have the methods that we need.  Long name...
@@ -95,7 +85,8 @@ typename MatType::elem_type SHADE::Optimize(FunctionType& function,
   }
 
   // Initialize helper variables.
-  fitnessValues.set_size(populationSize);
+  arma::Col<ElemType> fitnessDif;
+  fitnessDif.set_size(populationSize);
   ElemType lastBestFitness = DBL_MAX;
   BaseMatType bestElement;
   std::queue<ElemType> performanceHorizon;
@@ -104,15 +95,15 @@ typename MatType::elem_type SHADE::Optimize(FunctionType& function,
   // Controls early termination of the optimization process.
   bool terminate = false;
   arma::arma_rng::set_seed_random();
-  boxConstraint.RandomReseed(iterate);
-
+  boxConstraint.RandomReseed(iterate);//why just tje iterate ALIREZA CHECK
+  std::cout << "Need To be cheked ALIREZA";
   // Generate a population based on a Gaussian distribution around the given
   // starting point. Also finds the best element of the population.
   for (size_t i = 0; i < populationSize; i++)
   {
     //population[i].randn(iterate.n_rows, iterate.n_cols);
     //population[i] += iterate;
-
+      std::cout << "Need To be cheked ALIREZA";
     //random displacement from initial seed within box constraints
     population[i] = (BaseMatType&)boxConstraint.RandomDisplacement(iterate);
 
@@ -136,68 +127,54 @@ typename MatType::elem_type SHADE::Optimize(FunctionType& function,
   {
 
   	//For generating crossover rate SHADE SHADE SHADE SHADE SHADE SHADE
-      // Generate random indices
+    std::cout << "Alireza check if fitnessValues is updetaed correctly during the loop";
+    arma::umat goodChildren = arma::zeros<arma::umat>(populationSize);
       arma::uvec sorted_index = arma::sort_index(fitnessValues);
-
+      // Generate random indices
       arma::vec mem_rand_index = arma::floor(arma::randu<arma::vec>(pop_size) * memory_size);
       arma::vec mu_sf = memory_sf(arma::conv_to<arma::uvec>::from(mem_rand_index));
       arma::vec mu_cr = memory_cr(arma::conv_to<arma::uvec>::from(mem_rand_index));
       // Generate crossover rate
       arma::vec crS = mu_cr + 0.1 * arma::randn<arma::vec>(pop_size);
-      arma::umat goodChildren=arma::zeros<arma::umat>(populationSize);
-      
-      //arma::vec<bool> goodCR = arma::zeros<arma::vec<bool>>(populationSize);
-
-      
-      //goodF.set_size(populationSize);
-
-
-      // Find positions where mu_cr == -1
-      arma::uvec term_pos = arma::find(mu_cr == -1);
-      // Set elements of cr to 0 where mu_cr == -1
-      crS.elem(term_pos).fill(0);
-      // Clip cr values to [0, 1]
-      crS = arma::clamp(crS, 0.0, 1.0);
+      //arma::uvec term_pos = arma::find(mu_cr == -1);
+      //crS.elem(term_pos).fill(0);
+      crS.clamp(0.0, 1.0);
 
       // Generate scaling factor
       arma::vec sf = mu_sf + 0.1 * arma::tan(arma::datum::pi * (arma::randu<arma::vec>(pop_size) - 0.5));
-      // Find positions where sf is less than or equal to 0
       arma::uvec pos = arma::find(sf <= 0);
       // Regenerate sf for positions where sf is less than or equal to 0
       while (!pos.is_empty()) {
           sf.elem(pos) = mu_sf.elem(pos) + 0.1 * arma::tan(arma::datum::pi * (arma::randu<arma::vec>(pos.n_elem) - 0.5));
           pos = arma::find(sf <= 0);
       }
-      // Clip sf values to [0, 1]
       sf = arma::min(sf, 1.0);
 
-      
-
-      arma::uvec r0 = arma::linspace<arma::uvec>(0, pop_size-1, pop_size);
-      arma::uvec r1(pop_size);
-      arma::uvec r2(pop_size);
 
       std::vector<BaseMatType> popAll = population;
       popAll.insert(popAll.end(), archive_pop.begin(), archive_pop.end());
+
       //// Generate r1 and r2
+      arma::uvec r0 = arma::linspace<arma::uvec>(0, pop_size-1, pop_size);
+      arma::uvec r1(pop_size);
+      arma::uvec r2(pop_size);
       gnR1R2(pop_size, popAll.size(), r0, r1, r2);
 
-      // Calculate the number of solutions to choose (at least two best solutions)
+      //generate a vector of best solutions
+      // Calculate the number of best solutions to choose (at least two best solutions)
       int pNP = std::max(static_cast<int>(std::round(p_best_rate * pop_size)), 2);
       arma::uvec randindex = arma::randi<arma::uvec>(pop_size, arma::distr_param(0, pNP - 1));
-      //randindex = arma::max(randindex, 0);
 
-
-      std::vector<BaseMatType> pbest;
-      pbest.resize(pop_size);
-	  arma::vec pbestFitness = arma::zeros<arma::vec>(pop_size);
+      std::vector<BaseMatType> pbest(pop_size);
+	  //arma::vec pbestFitness = arma::zeros<arma::vec>(pop_size);
       for (std::size_t i = 0; i < pop_size; ++i) {
-          std::size_t index = randindex[i];
-          if (index < sorted_index.size()) {
-              pbest[i] = population[sorted_index[index]];
-              pbestFitness[i] = fitnessValues[sorted_index[index]];
+          //std::size_t index = randindex[i];
+          if (randindex[i] < sorted_index.size()) {
+              pbest[i] = population[sorted_index[randindex[i]]];
+              //pbestFitness[i] = fitnessValues[sorted_index[randindex[i]]];
           }
       }
+
     std::vector<BaseMatType> vi(pop_size, BaseMatType(problem_size, 0.0));
     for (int i = 0; i < pop_size; ++i) {
 		// Generate mutant vector
@@ -228,6 +205,9 @@ typename MatType::elem_type SHADE::Optimize(FunctionType& function,
 
   	//SHADE SHADE SHADE SHADE SHADE SHADE
 
+    // Archiving data SHADE SHADE SHADE SHADE SHADE SHADE
+    std::vector<BaseMatType> popToArchive(0, BaseMatType(problem_size, 0.0));
+    arma::Col<ElemType> funvalueToArchive;
     // Generate new population based on /best/1/bin strategy.
     #pragma omp parallel for num_threads(8)
     for (int member = 0; member < populationSize; member++)
@@ -261,19 +241,95 @@ typename MatType::elem_type SHADE::Optimize(FunctionType& function,
         #pragma omp critical 
         terminate |= Callback::StepTaken(*this, function, iterate,callbacks...);
       }
+      else
+      {
+          popToArchive.push_back(mutant);
+      	  funvalueToArchive.insert_rows(funvalueToArchive.n_rows, mutantValue);
+      }
 
       fitnessValues[member] = iterateValue;
       population[member] = iterate;
 
     }
-
-
     arma::vec goodCr = crS.elem(arma::find(goodChildren));
     arma::vec goodSf = sf.elem(arma::find(goodChildren));
     arma::vec dif_val = fitnessDif.elem(arma::find(goodChildren));
 
-    std::cout << "updateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchiveupdateArchive"<< std::endl;
-    std::cout << "      [fitness, I] = min([fitness, children_fitness], [], 2)fnalksjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj" << std::endl;
+  	//archiveSHADEUPDATEUPDATEUPDATEUPDATEUPDATEUPDATEUPDATEUPDATEUPDATEUPDATEU
+    std::vector<BaseMatType> tempArcive = archive_pop;
+    tempArcive.insert(tempArcive.end(), popToArchive.begin(), popToArchive.end());
+    arma::Col<ElemType> tempfunvalues = arma::join_cols(archive_funvalues, funvalueToArchive);
+    //tempfunvalues.insert_rows(tempfunvalues.n_rows, funvalueToArchive);
+
+    std::cout<< "Aliiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"<<std::endl;
+
+    for (int i = 0; i < archive_pop.size(); ++i)
+    {
+        std::cout << "archive_pop." << i << "=" << archive_pop[i] << std::endl;
+    }
+    for (int i = 0; i < popToArchive.size(); ++i)
+    {
+        std::cout << "popToArchive." << i << "=" << popToArchive[i] << std::endl;
+    }
+    for (int i = 0; i < tempArcive.size(); ++i)
+    {
+        std::cout << "tempArcive." << i << "=" << tempArcive[i] << std::endl;
+    }
+
+    std::unordered_set<std::string> uniqueIdentifiers;
+    std::vector<bool> hasDuplicates(tempArcive.size(), false);
+
+    for (std::size_t i = 0; i < tempArcive.size(); ++i) {
+        const BaseMatType& element = tempArcive[i];
+
+        // Convert BaseMatType to a unique identifier (string ).
+        std::ostringstream oss;
+        oss << element;  // You might want to use a more appropriate serialization method based on your needs.
+        std::string identifier = oss.str();
+
+        // Check for duplicates.
+        if (!uniqueIdentifiers.insert(identifier).second) {
+            hasDuplicates[i] = true;  // Set the corresponding flag to true.
+            if(i<archive_pop.size())
+            {
+                throw std::logic_error("SHADE::Optimize(): there is a identical member in SHADE Archive");
+			}
+        }
+        else
+        {
+            if (i >= archive_pop.size() && archive_pop.size() < archive_NP)
+            {
+                archive_pop.push_back(element);
+				archive_funvalues.insert_rows(archive_funvalues.n_rows, tempfunvalues[i]);
+			}
+            else if (i >= archive_pop.size() && archive_pop.size() == archive_NP)
+            {
+	            int randIndex = arma::randi<arma::uvec>(1, arma::distr_param(0, archive_NP - 1))[0];
+				archive_pop[randIndex] = element;
+                archive_funvalues[randIndex] = tempfunvalues[i];
+            }
+            else if (archive_pop.size() > archive_NP)
+            {
+	            throw std::logic_error("SHADE::Optimize(): Archive exceeds the maximum size");
+            }
+        }
+    }
+
+
+  //  for (const BaseMatType& element : tempArcive)
+  //  {
+  //  	std::ostringstream oss;
+		//oss << element;  // You might want to use a more appropriate serialization method based on your needs.
+		//std::string identifier = oss.str();
+		//// Check for duplicates.
+		//if (!uniqueIdentifiers.insert(identifier).second)
+		//{
+  //          archive_pop.push_back(element);
+		//}
+
+  //  }
+
+    	//archiveSHADEUPDATEUPDATEUPDATE
     // Append bestFitness to performanceHorizon.
     size_t id_min = fitnessValues.index_min();
     ElemType min_objective = fitnessValues[id_min];
@@ -370,6 +426,12 @@ inline void SHADE::gnR1R2(int NP1, int NP2, const arma::uvec& r0, arma::uvec& r1
     }
 
 }
+
+struct Archive {
+    arma::mat pop;
+    arma::mat funvalues;
+    size_t NP; // Assuming NP is the archive size
+};
 
 inline Archive updateArchive(const Archive& archive, const arma::mat& pop, const arma::mat& funvalue) {
     // Check if the archive size is zero, then return
